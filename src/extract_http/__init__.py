cb1a8c46b2a4a531f32584b1628392c787f74648
denwong47 <85551679+denwong47@users.noreply.github.com>
2022-01-06 22:55:59 +0000

    hierachy change to match README

--- src/extract_http/__init__.py
+++ src/extract_http/__init__.py
@@ -1,8 +1,8 @@
-import extract_http.bin
-import extract_http.exceptions
-import extract_http.extract
-import extract_http.html_node
-import extract_http.record_dict
-import extract_http.transform
+import extract_http.bin as bin
+import extract_http.exceptions as exceptions
+import extract_http.extract as extract
+import extract_http.html_node as html_node
+import extract_http.record_dict as record_dict
+import extract_http.transform as transform
 
-import extract_http.defaults
+import extract_http.defaults as defaults
